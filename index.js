321b2f3d50d493e91344ee820e5c399402b9bcf3
Hannah Bradley <hannahthesailor@yahoo.com>
2024-01-30 01:21:09 -0500

    phase-1-javascript-functions-continued.md

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun());
+  console.log(saturdayFun('bathe my dog'));
+
+  function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+    console.log(mondayWork());
+    console,log(mondayWork('work from home'))
+  }
+
+  function wrapAdjective(symbol) {
+    return function (adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    };
+  }
+  
+  let result1 = wrapAdjective("*");
+  let emphatic1 = result1("a hard worker");
+  console.log(emphatic1);
+  
+  let result2 = wrapAdjective("||");
+  let emphatic2 = result2("a dedicated programmer");
+  console.log(emphatic2);
